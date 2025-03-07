d70c661b4cac6a3f3f6fd66d661f07c1605c0e93
Adam Laycock <adam@alaycock.co.uk>
2025-03-07 08:29:12 +0000

    feat: send files to the controller

--- piper.sh
+++ piper.sh
@@ -2,4 +2,7 @@
 text=$(echo $1 | jq -r '.text')
 target=$(echo $1 | jq -r '.target')
 echo $text | /var/piper/piper --model /var/piper/model.onnx --output-file /output/$target
+curl \
+  -F "sound=@/output/$target" \
+  $CONTROLLER/tts-api/upload
 echo '{"status": "ok"}'
