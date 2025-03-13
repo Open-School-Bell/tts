2b3d7d1bcb3d5310371ed071123b148053b01a16
Adam Laycock <adam@alaycock.co.uk>
2025-03-13 21:29:23 +0000

    feat: return the generated audio as the response

--- piper.sh
+++ piper.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 text=$(echo $1 | jq -r '.text')
 target=$(echo $1 | jq -r '.target')
-echo $text | /var/piper/piper --model /var/piper/model.onnx --output-file /output/$target
-curl \
-  -F "sound=@/output/$target" \
-  $CONTROLLER/tts-api/upload
-echo '{"status": "ok"}'
+echo $text | /var/piper/piper --model /var/piper/model.onnx --output-file /output/$target &> /dev/null
+cat /output/$target
